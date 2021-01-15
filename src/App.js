46e7ee5b809f0dbda5761cacfc6a39e251b549f6
Klaus Kerstges <klaus.kerstges@gmail.com>
2021-01-15 17:59:54 +0100

    Serach users via context API

--- src/App.js
+++ src/App.js
@@ -18,17 +18,6 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [alert, setAlert] = useState(null);
 
-  // Search GitHub users
-  const searchUsers = async (searchTerm) => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${searchTerm}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    setUsers(res.data.items);
-    setLoading(false);
-  };
-
   // Get single GibHub user
   const getUser = async (username) => {
     setLoading(true);
@@ -81,7 +70,6 @@ const App = () => {
                 render={(props) => (
                   <Fragment>
                     <Search
-                      searchUsers={searchUsers}
                       clearUsers={clearUsers}
                       showClear={users.length > 0}
                       setAlert={showAlert}
