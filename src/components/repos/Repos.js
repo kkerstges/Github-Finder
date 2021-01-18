63c39616e9d9fb177cc5deb20c4bb8986060fa34
Klaus Kerstges <klaus.kerstges@gmail.com>
2021-01-18 10:57:43 +0100

    minor prop fix

--- src/components/repos/Repos.js
+++ src/components/repos/Repos.js
@@ -1,7 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import RepoItem from "./RepoItem";
-import GithubContext from "../../context/github/githubContext";
 
 const Repos = ({ repos }) => {
   return repos.map((repo) => <RepoItem repo={repo} key={repo.id} />);
